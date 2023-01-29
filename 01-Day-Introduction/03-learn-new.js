ecf8af9360529ea246f4426b5a098f2f4159153c
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-01-30 00:13:55 +0530

    Update 03-learn-new.js

--- 01-Day-Introduction/03-learn-new.js
+++ 01-Day-Introduction/03-learn-new.js
@@ -20,20 +20,3 @@ for(let i = 0; i<size; i++){
     console.log(user_name[i]);
 }
 
-//4 reverse the string
-
-let start = 0;
-let end = user_name.length;
-
-while(start>end){
-    let temp = user_name[start];
-    user_name[start] = user_name[end];
-    user_name[end] = temp;
-
-    start ++;
-    end --;
-}
-
-for(let i = 0; i<size; i++){
-    console.log(user_name[i]);
-}
